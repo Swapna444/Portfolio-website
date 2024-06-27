bc35d646afac028c9fb874e728acb9b7ae84ce8b
Swapna444 <swapraikar444@gmail.com>
2024-06-27 16:12:07 +0530

    all files are uploaded

---
+++ script.js
@@ -0,0 +1,22 @@
+//function for profile photo
+document.getElementById('show'),addEventListener('click',function(){this.document.getElementById('imageprof').classList.remove('hidden');});
+
+function showimg1(){
+    const image1= document.getElementById('cert1');
+    image1.classList.add('show')
+}
+
+function showimg2(){
+    const image2= document.getElementById('cert2');
+    image2.classList.add('show')
+}
+
+function showimg3(){
+    const image3= document.getElementById('cert3');
+    image3.classList.add('show')
+}
+
+function showimg4(){
+    const image4= document.getElementById('cert4');
+    image4.classList.add('show')
+}
